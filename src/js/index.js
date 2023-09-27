5da7f9a896b8003576a3e8e11342633a6f69d632
Ilya Pyatnichuk <zein3288@gmail.com>
2023-09-27 14:40:53 +0300

    form

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,15 @@ import autoCompleteFunc from './modules/autoCompleteFunc';
 autoCompleteFunc();
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
+
+import { easepick, TimePlugin } from '@easepick/bundle';
+
+const picker = new easepick.create({
+  element: document.getElementById('datepicker'),
+  css: [
+    'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+    'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
+  ],
+  format: 'HH:mm, DD/MM/YY',
+  plugins: [TimePlugin],
+});
