e6f51347dd3c2a8f4c3c77aa46c763ce84e3ca0d
Ilya Pyatnichuk <zein3288@gmail.com>
2023-09-28 21:46:47 +0300

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,20 @@
 import autoCompleteFunc from './modules/autoCompleteFunc';
+import datePicker from './modules/datepicker';
 
 autoCompleteFunc();
+datePicker();
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});
+
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
-
-import { easepick, TimePlugin } from '@easepick/bundle';
-
-const picker = new easepick.create({
-  element: document.getElementById('datepicker'),
-  css: [
-    'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
-    'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
-  ],
-  format: 'HH:mm, DD/MM/YY',
-  plugins: [TimePlugin],
-});
---
+++ src/js/modules/datepicker.js
@@ -0,0 +1,17 @@
+import { easepick, TimePlugin } from '@easepick/bundle';
+
+function datePicker() {
+  const picker = new easepick.create({
+    element: document.getElementById('datepicker'),
+    css: [
+      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+      'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
+    ],
+    format: 'HH:mm, DD/MM/YY',
+    plugins: [TimePlugin],
+  });
+
+  return picker;
+}
+
+export default datePicker;
