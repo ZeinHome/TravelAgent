66b484f184270600ba579639d5e4836dc14775e9
Ilya Pyatnichuk <zein3288@gmail.com>
2023-10-02 16:46:48 +0300

    end
    надеюсь что это ласт комент

--- src/js/modules/swiper-destinations.js
+++ src/js/modules/swiper-destinations.js
@@ -3,11 +3,20 @@ import 'swiper/css/bundle';
 
 function swiperCard() {
   const swiper = new Swiper('.swiper-destinations', {
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 32,
     navigation: {
       nextEl: '#sliderNext',
       prevEl: '#sliderPrev',
+    },
+
+    breakpoints: {
+      425: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
     },
   });
 }
--- src/js/modules/swiper-testimonials.js
+++ src/js/modules/swiper-testimonials.js
@@ -1,11 +1,42 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-function swiperCardTestiomonials() {
-  const swiper = new Swiper('.testimonials__col--one', {
+let swiper, swiperHorizontal;
+
+const breakpoints = window.matchMedia('(max-width: 1023px)');
+
+const breakpointsChecker = function () {
+  if (breakpoints.matches === true) {
+    swiperHorizontal = new Swiper('.testimonials__col--horiontal', {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      spaceBetween: 32,
+      grabCursor: true,
+      a11y: false,
+      freeMode: true,
+      speed: 2000,
+      loop: true,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+      },
+    });
+
+    if (swiper !== undefined) swiper.destroy(true, true);
+
+    return;
+  }
+
+  swiper = new Swiper('.testimonials__col--one', {
     direction: 'vertical',
     slidesPerView: 'auto',
-    spaceBetween: 32,
+    spaceBetween: 0,
     grabCursor: true,
     a11y: false,
     freeMode: true,
@@ -16,6 +47,12 @@ function swiperCardTestiomonials() {
       disableOnInteraction: false,
     },
   });
-}
 
-export default swiperCardTestiomonials;
+  if (swiperHorizontal !== undefined) swiperHorizontal.destroy(true, true);
+
+  return;
+};
+
+breakpoints.addListener(breakpointsChecker);
+
+export default breakpointsChecker;
